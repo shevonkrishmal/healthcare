e77f502a1c5f4038dba718f5d08284edd31995b2
shevon krishmal <it19107356@my.sliit.lk>
2020-10-04 20:35:31 +0530

    IT19107356

---
+++ app/src/main/java/com/example/healthcare/Doctor_list.java
@@ -0,0 +1,20 @@
+package com.example.healthcare;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Doctor_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_list);
+
+
+
+
+
+
+    }
+}
